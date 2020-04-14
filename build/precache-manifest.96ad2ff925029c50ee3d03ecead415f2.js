9be612573aa0bd4fcad50b9daa6d07238ceb0dc0
marcelobrgomes <marcelobrgomes@gmail.com>
2020-04-14 13:01:36 -0300

    testing deploy

---
+++ build/precache-manifest.96ad2ff925029c50ee3d03ecead415f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "003c24a6ca3173caf18e1f3bcfeedfd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6126f6042cc1ede54b4",
+    "url": "/static/css/main.14e3ac9a.chunk.css"
+  },
+  {
+    "revision": "27a7455f07c6dee95b8f",
+    "url": "/static/js/2.6a0a4b74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a0a4b74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6126f6042cc1ede54b4",
+    "url": "/static/js/main.02929cf2.chunk.js"
+  },
+  {
+    "revision": "cba5ac7c65879057899a",
+    "url": "/static/js/runtime-main.7ad27f0e.js"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/static/media/Roboto-Black.5ebb24ee.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
